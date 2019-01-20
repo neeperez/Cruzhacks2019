cdefd4fc69da6f063079eddd275cc98c1ddb1c6c
Nelson Perez <neeperez@ucsc.edu>
2019-01-19 17:38:56 -0800

    Nice app

--- ChromeFilter.js
+++ ChromeFilter.js
@@ -1,33 +1,64 @@
 // So first we wanna find out how to read through all of the text items
+function start() {
+    var AllElements = document.getElementsByTagName("*");
+    
+    /*** DISCLAIMER ***/
+    /* WARNING: HERE COMES A HUGE LINE OF POTENTIALLY OFFENSIVE TEXT */
+    /* THIS IS USED SOLEY FOR THE PURPOSE OF FILTERING OUT SOME OFFENSIVE */
+    /* TEXT AND REPLACING THEM WITH NON-OFFENSIVE WORDS THAT DEFLATE THE */
+    /* WHOLE POINT OF THE ORIGINAL POST.  */
+    var allWords = {
+        fuck: "frick",
+        fucking: "great",
+        damn: "damb",
+        dammit: "dambit",
+        damnit: "dambnit",
+        shit: "shoot, but for poop",
+        shid: "shood",
+        shitter: "latrine",
+        phuck: "phrick",
+        fucker: "hugger",
+        phucker: "hugger",
+        fucking: "great",
+        phucking: "good",
+        piss: "peepee",
+        pissed: "grumpy",
+        pisser: "restroom",
+        bitch: "doggy",
+        bitching: "mouthing off",
+        bitchy: "naughty",
+        bastard: "villain",
+        ass: "potato salad",
+        cunt: "kitty cat",
+        dick: "badillywillyshashilly",
+        cock: "male chicken man",
+        
 
-var AllElements = document.getElementsByTagName("*");
+    };
+    for(var i = 0; i < AllElements.length; i++){
+        var CurrentElement = AllElements[i];
 
+        for(var k = 0; k < CurrentElement.childNodes.length; k++) {
+            var CurrentNode = CurrentElement.childNodes[k];
+            console.log(CurrentNode);
+            if(CurrentNode.nodeType === 3){
+                //If we have a text thingy, then start to replace it
+               // var node = CurrentNode.nodeValue;
 
-for(var i = 0; i < AllElements.length; i++){
-    var CurrentElement = AllElements[i];
+                var text = CurrentNode.nodeValue;
+                console.log(text);
 
-    for(var k; k < CurrentElement.childNodes.length; k++) {
-        var CurrentNode = CurrentElement.childNodes[k];
+                if(text != undefined){
+                    var replaceText = text.replace(/damn|dammit|damnit|shit|shid|shitter|phuck|phucking|piss|pissed|pisser|bitch|b!tch|bitching|bitchy|bastard|ass|cunt|dick|fuck|fucking|shit/gi, function(word) {
+                        word = word.toLowerCase();
+                        return allWords[word];
+                    });
+                    
 
-        if(CurrentNode.nodeType == 3){
-            //If we have a text thingy, then start to replace it
-            var text = CurrentNode.nodeValue;
-
-            var allWords = {
-                fuck: "frick",
-                fucking: "great",
-            }
-            /* WARNING: HERE COMES A HUGE LINE OF POTENTIALLY OFFENSIVE TEXT */
-            /* THIS IS USED SOLEY FOR THE PURPOSE OF FILTERING OUT SOME OFFENSIVE */
-            /* TEXT AND REPLACING THEM WITH NON-OFFENSIVE WORDS THAT DEFLATE THE */
-            /* WHOLE POINT OF THE ORIGINAL POST.  */
-            var replaceText = text.replace(/fuck|fucking/gi, function(word) {
-
-            };
-            
-
-            if(replaceText != text){
-                CurrentElement.replaceChild(document.createTextNode(replaceText), text);
+                    if(replaceText != text){
+                        CurrentElement.replaceChild(document.createTextNode(replaceText), CurrentNode);
+                    }
+                }
             }
         }
     }
